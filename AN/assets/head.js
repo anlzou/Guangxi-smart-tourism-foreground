3d7c9162c28ed42da8d30fdcd8e858ebc9c1be51
anlzou <599502931@qq.com>
2020-06-15 15:07:24 +0800

    html 转换 javascript 静态部署测试

---
+++ AN/assets/head.js
@@ -0,0 +1,37 @@
+/*
+ * @Date        : 2020-06-15 15:03:47
+ * @LastEditors : anlzou
+ * @Github      : https://github.com/anlzou
+ * @LastEditTime: 2020-06-15 15:05:05
+ * @FilePath    : \sparks-of-fire\Guangxi-smart-Tourism-foreground\AN\assets\head.js
+ * @Describe    : 
+ */
+document.writeln("<title>广西智慧旅游平台</title>");
+document.writeln("	<!-- Custom Theme files -->");
+document.writeln("	<meta name=\'viewport\' content=\'width=device-width, initial-scale=1\'>");
+document.writeln("	<meta http-equiv=\'Content-Type\' content=\'text/html; charset=utf-8\' />");
+document.writeln("	<meta name=\'keywords\' content=\'Govihar Responsive web template, Bootstrap Web Templates, Flat Web Templates, Andriod Compatible web template, ");
+document.writeln("	Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyErricsson, Motorola web design\' />");
+document.writeln("	<script");
+document.writeln("		type=\'application/x-javascript\'> addEventListener(\'load\', function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>");
+document.writeln("	<!-- //Custom Theme files -->");
+document.writeln("	<link href=\'css/bootstrap.css\' type=\'text/css\' rel=\'stylesheet\' media=\'all\'>");
+document.writeln("	<link href=\'css/style.css\' type=\'text/css\' rel=\'stylesheet\' media=\'all\'>");
+document.writeln("	<link rel=\'stylesheet\' href=\'css/flexslider.css\' type=\'text/css\' media=\'screen\' />");
+document.writeln("	<link type=\'text/css\' rel=\'stylesheet\' href=\'css/JFFormStyle-1.css\' />");
+document.writeln("	<!-- js -->");
+document.writeln("	<script src=\'js/jquery.min.js\'></script>");
+document.writeln("	<script src=\'js/modernizr.custom.js\'></script>");
+document.writeln("	<!-- //js -->");
+document.writeln("	<script type=\'text/javascript\'>");
+document.writeln("		$(document).ready(function () {");
+document.writeln("			$(\'#horizontalTab\').easyResponsiveTabs({");
+document.writeln("				type: \'default\', //Types: default, vertical, accordion           ");
+document.writeln("				width: \'auto\', //auto or any width like 600px");
+document.writeln("				fit: true   // 100% fit in a container");
+document.writeln("			});");
+document.writeln("		});");
+document.writeln("	</script>");
+document.writeln("	<!--pop-up-->");
+document.writeln("	<script src=\'js/menu_jquery.js\'></script>");
+document.writeln("	<!--//pop-up-->");
